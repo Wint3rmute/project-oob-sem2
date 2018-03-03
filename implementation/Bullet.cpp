c8a589a1042dda30af737552259373ab080a2d7c
wint3rmute <mateusz.baczek1998@gmail.com>
2018-03-03 19:19:35 +0100

    code clearing vol2

--- implementation/Bullet.cpp
+++ implementation/Bullet.cpp
@@ -3,22 +3,24 @@
 #include <SFML/System.hpp>
 #include <math.h>
 
-Bullet::Bullet (int pos_x, int pos_y, float rotation) : shape(BULLET_SIZE) {
+Bullet :: Bullet (int pos_x, int pos_y, float rotation) : shape(BULLET_SIZE) {
 
-		float direction = rotation * DEG_2_RAD;
-		direction_x = cos(direction) * BULLET_SPEED;
-		direction_y = sin(direction) * BULLET_SPEED;
+	float direction = rotation * DEG_2_RAD;
+	direction_x = cos(direction) * BULLET_SPEED;
+	direction_y = sin(direction) * BULLET_SPEED;
 	
 }
 
 
-void Bullet :: draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    	target.draw(shape, states);
+void Bullet :: draw(sf::RenderTarget & target, sf::RenderStates states) const {
+
+	target.draw(shape, states);
+
 }
 
 void Bullet :: simulate() {
 
-		shape.move(direction_x, direction_y);
+	shape.move(direction_x, direction_y);
 
 	
 }
--- implementation/Plane.cpp
+++ implementation/Plane.cpp
@@ -32,54 +32,57 @@ Plane::Plane (int pos_x, int pos_y, float rotation) {
 
 
 void Plane :: draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    	target.draw(shape, states);
+
+	target.draw(shape, states);
+	
 }
 
 void Plane :: simulate() {
 
 
-		float angle_rad = shape.getRotation() * DEG_2_RAD;
+	float angle_rad = shape.getRotation() * DEG_2_RAD;
 
-		shape.move(cos(angle_rad) * speed , sin(angle_rad) * speed);
+	shape.move(cos(angle_rad) * speed , sin(angle_rad) * speed);
 
-		speed += sin(angle_rad) * ACCELERATION_MULTIPLIER;
+	speed += sin(angle_rad) * ACCELERATION_MULTIPLIER;
 
-		if( shape.getPosition().y <= -TELEPORTATION_TRIGGER_OFFSET)
-			shape.move(0, GAME_WINDOW_HEIGHT + TELEPORTATION_TRIGGER_OFFSET);
-		if( shape.getPosition().y >= GAME_WINDOW_HEIGHT + TELEPORTATION_TRIGGER_OFFSET)
-		{
+	if ( shape.getPosition().y <= -TELEPORTATION_TRIGGER_OFFSET)
+		shape.move(0, GAME_WINDOW_HEIGHT + TELEPORTATION_TRIGGER_OFFSET);
 
-				cout << shape.getPosition().y << " " << GAME_WINDOW_HEIGHT << " BOOM" << endl;
-				shape.move(0, -GAME_WINDOW_HEIGHT - TELEPORTATION_TRIGGER_OFFSET);
-		}
-		if( shape.getPosition().x <= -TELEPORTATION_TRIGGER_OFFSET)
-			 	shape.move(GAME_WINDOW_WIDTH + TELEPORTATION_TRIGGER_OFFSET,0);
-		if( shape.getPosition().x >= GAME_WINDOW_WIDTH + TELEPORTATION_TRIGGER_OFFSET)
-						shape.move(-GAME_WINDOW_WIDTH - TELEPORTATION_TRIGGER_OFFSET, 0);
+	if ( shape.getPosition().y >= GAME_WINDOW_HEIGHT + TELEPORTATION_TRIGGER_OFFSET)
+		shape.move(0, -GAME_WINDOW_HEIGHT - TELEPORTATION_TRIGGER_OFFSET);
+	
+	if ( shape.getPosition().x <= -TELEPORTATION_TRIGGER_OFFSET )
+	 	shape.move(GAME_WINDOW_WIDTH + TELEPORTATION_TRIGGER_OFFSET,0);	
 
-		if (speed > MAX_SPEED)
-			speed = MAX_SPEED;
-		else if (speed < MIN_SPEED)
-			speed = MIN_SPEED;
+	if ( shape.getPosition().x >= GAME_WINDOW_WIDTH + TELEPORTATION_TRIGGER_OFFSET)
+		shape.move(-GAME_WINDOW_WIDTH - TELEPORTATION_TRIGGER_OFFSET, 0) ;
 
-		//std::cout << speed << std::endl;
-		steer();
+
+	if (speed > MAX_SPEED)
+		speed = MAX_SPEED;
+
+	else if (speed < MIN_SPEED)
+		speed = MIN_SPEED;
+
+
+	//std::cout << speed << std::endl;
+	steer();
 
 }
 
 void Plane :: steer() {
 
-		if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left)) {
-	    	turn(LEFT);
+	if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left)) 
+    	turn(LEFT);
 
-		} else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right)) {
-
-			turn(RIGHT);
-		}
+	else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right))
+		turn(RIGHT);
 
 }
 
 void Plane :: turn(DIRECTION direction) {
 
 	shape.rotate(direction * ROTATION_SPEED * speed);
+
 }
