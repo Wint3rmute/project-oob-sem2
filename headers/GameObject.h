d688fc776f7ab4291a3c524d419f48b2ffd60cc0
wint3rmute <mateusz.baczek1998@gmail.com>
2018-03-04 10:27:24 +0100

    bullshit

--- headers/GameObject.h
+++ headers/GameObject.h
@@ -4,8 +4,9 @@
 
 class GameObject : public sf::Drawable {
 
+	bool collisionsAffected = false;
+
 	virtual void simulate() = 0;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
-    
 	
 };
