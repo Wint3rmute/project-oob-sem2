51ceced73d3cf73e3b39c8786e88b479ca3580e8
wint3rmute <mateusz.baczek1998@gmail.com>
2018-03-04 13:45:07 +0100

    fucking compilation script bug

--- compile.sh
+++ compile.sh
@@ -3,7 +3,9 @@ g++ \
 	-I .  \
 	main.cpp \
        	headers/*.h \
+	headers/skills/*.h \
 	implementation/*.cpp \
+	implementation/skills/*.cpp \
 	utils/* \
 	-lsfml-graphics -lsfml-window -lsfml-system \
 	-o build/app \
