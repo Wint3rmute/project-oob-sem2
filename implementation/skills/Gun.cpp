7621fdc4b20863ec114e7184c713f49075bc52e2
Szymon <szymon.filipiak126@gmail.com>
2018-03-04 23:13:51 +0100

    fix

--- implementation/skills/Gun.cpp
+++ implementation/skills/Gun.cpp
@@ -3,7 +3,7 @@
 
 void Gun :: activate(Plane & plane)
 {
-
+  
   Bullet * bullet = new Bullet(0,0,45);//( plane.getPosition().x, plane.getPosition().y, plane.getRotation());
   GameEngine::addObject(bullet);
 
