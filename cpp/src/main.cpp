3d9386fbf29112c79f23fad0fd4114e7becc871f
Wint3rmute <mateusz.baczek1998@gmail.com>
2018-06-08 12:27:22 +0200

    main page for the doxygen

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -11,6 +11,17 @@
 #include <iostream>
 #include <ctime>
 
+/*! \mainpage Temat projektu: Trenowanie sieci neuronowych w grze zręcznościowej
+ *
+ * Autorzy:
+ * - Szymon Filipiak
+ * - Mateusz Bączek
+ *
+ * W4 Informatyka, semestr letni 2017-2018
+ *
+ * Szczegółowy opis projektu zawarty jest w sprawozdaniu.
+ */
+
 
 int main(int argc, char* argv[])
 {
